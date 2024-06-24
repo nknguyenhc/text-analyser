82408d74fe33c26a15eae0ca2c56795bbc7c0f7a
nknguyenhc <87511888+nknguyenhc@users.noreply.github.com>
2024-06-24 20:39:20 +0800

    Add app skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import { Button, Grid, Typography } from "@mui/material";
+import { ChangeEvent, useCallback, useState } from "react";
+import "./App.scss";
 
-function App() {
+const App = () => {
+  const [file, setFile] = useState<File | null>(null);
+  const handleFileChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    const selectedFile = e.target.files?.[0];
+    if (selectedFile) {
+      setFile(selectedFile);
+    }
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <Typography variant="h4" align="center" sx={{ padding: "50px" }}>
+        Analyse your Telegram texts!
+      </Typography>
+      <Grid container alignItems="center" justifyContent="center" spacing={2}>
+        <label htmlFor="file-upload">
+          <Button component="span">Upload File</Button>
+        </label>
+        <Typography sx={{ width: "300px", textAlign: "center" }}>
+          {file ? file.name : "No file selected"}
+        </Typography>
+        <input
+          id="file-upload"
+          type="file"
+          accept=".html"
+          onChange={handleFileChange}
+          style={{ display: "none" }}
+        />
+      </Grid>
     </div>
   );
-}
+};
 
 export default App;
